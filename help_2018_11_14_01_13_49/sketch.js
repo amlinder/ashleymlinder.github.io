696e0e2023bd5a44fcdc49244a022a62a1aea3da
ashleymlinder <amlinder@yahoo.com>
2018-11-20 17:14:56 -0800

    just fix it

--- help_2018_11_14_01_13_49/sketch.js
+++ help_2018_11_14_01_13_49/sketch.js
@@ -1,17 +1,9 @@
 var b = 'help'
 var c = "i'm stuck in the void"
-var d = 'with no way out'
-var img= 'stick.png'
 
-
-function preload() {
-  img= loadImage("stick.png")
-}
-function setup(){
-
-  createCanvas(1200, 700)
+function setup() {
+  createCanvas(600, 600);
   noStroke();
-  noCursor();
 }
 
 function draw() {
@@ -19,20 +11,13 @@ function draw() {
   var y = mouseY;
   var ix = width - mouseX; // Inverse X
   var iy = height - mouseY; // Inverse Y
-
-  background(0);
   background(126);
-  image(img,mouseX,mouseY);
-  //bubbles
   fill(255, 150);
-  ellipse(x, height / 3, y, y);
+  ellipse(x, height / 2, y, y);
   fill(0, 159);
   ellipse(ix, height / 2, iy, iy);
-  //text
   fill(240);
   text(b, 15, 15, 50, 50);
   fill(240);
-  text(c, 590, 350, 50, 50);
-  fill(240);
-  text(d, 1100, 660, 50, 50);
+  text(c, 550, 550, 50, 50);
 }
