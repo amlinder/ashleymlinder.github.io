f2e206062410450c522b0ad926af1010777223a5
ashleymlinder <amlinder@yahoo.com>
2018-11-20 17:03:34 -0800

    what is happening

--- CodeArt/help_2018_11_14_01_13_49/sketch.js
+++ CodeArt/help_2018_11_14_01_13_49/sketch.js
@@ -1,9 +1,17 @@
 var b = 'help'
 var c = "i'm stuck in the void"
+var d = 'with no way out'
+var img= 'stick.png'
 
-function setup() {
-  createCanvas(600, 600);
+
+function preload() {
+  img= loadImage("stick.png")
+}
+function setup(){
+
+  createCanvas(1200, 700)
   noStroke();
+  noCursor();
 }
 
 function draw() {
@@ -11,13 +19,20 @@ function draw() {
   var y = mouseY;
   var ix = width - mouseX; // Inverse X
   var iy = height - mouseY; // Inverse Y
+
+  background(0);
   background(126);
+  image(img,mouseX,mouseY);
+  //bubbles
   fill(255, 150);
-  ellipse(x, height / 2, y, y);
+  ellipse(x, height / 3, y, y);
   fill(0, 159);
   ellipse(ix, height / 2, iy, iy);
+  //text
   fill(240);
   text(b, 15, 15, 50, 50);
   fill(240);
-  text(c, 550, 550, 50, 50);
+  text(c, 590, 350, 50, 50);
+  fill(240);
+  text(d, 1100, 660, 50, 50);
 }
